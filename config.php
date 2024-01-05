e6eaf8c4ab405901e1f2fb86843d7d5c782abb81
Kosche1 <zedrickpiolocastro@gmail.com>
2024-01-05 10:29:46 +0800

    added dbconn

--- config.php
+++ config.php
@@ -2,7 +2,12 @@
 $dbhost = 'delta.optiklink.com';
 $dbuser = 'u126067_6YObtWKgqn';
 $dbname = 's126067_student';
-$dbpass = 's126067_student';
+$dbpass = '!yQ4Q2@Da6BB!8VWIhLcKKMw';
 
+ $dbconn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ if($dbconn){
+
+    echo "success";
+ }
 
 ?>
